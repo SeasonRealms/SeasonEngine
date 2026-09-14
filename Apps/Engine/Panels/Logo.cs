c336415b898214fbabbd86ae0ff59d8244953684
Joyming <leming.cen@live.com>
2026-09-14 14:50:10 +0800

    small features

--- Apps/Engine/Panels/Logo.cs
+++ Apps/Engine/Panels/Logo.cs
@@ -8,6 +8,8 @@ internal class Logo : Panel
 {
     internal Sprite2D sprite2D;
 
+    internal bool IsRecording = false;
+
     internal Logo()
     {
         RenderDomain = Season.Controls.RenderDomain.Overlay;
@@ -19,15 +21,27 @@ internal class Logo : Panel
             {
                 if (App.Instance.Mode is Mode.Play)
                 {
-                    //// 开始
-                    //await DeviceServices.Recorder.Start(new RecordSessionOptions { FramesPerSecond = 30 });
+                    if (IsRecording)
+                    {
+                        var result = await DeviceServices.Recorder.Stop();
 
-                    //await Task.Delay(TimeSpan.FromSeconds(10));
+                        var name = Path.GetFileName(result.FilePath);
 
-                    //// 结束
-                    //var result = await DeviceServices.Recorder.Stop();
-                    //// result.FilePath   → 输出的 mp4 路径
-                    //// result.Stats      → 掉帧诚实度量（见下）
+                        if (StorageService.TryGetBytes(StorageService.DirectoryBase, name, out byte[] bytes, out string errMsg))
+                        {
+                            DeviceServices.Download.DownloadDel(null, name);
+                            DeviceServices.Download.DownloadSave(null, name, bytes, true);
+                        }
+                    }
+                    else
+                    {
+                        IsRecording = true;
+
+                        await DeviceServices.Recorder.Start(new RecordSessionOptions { FramesPerSecond = 60 });
+                    }
+
+                    // result.FilePath   → Output mp4 path
+                    // result.Stats      → Frame Drop Honesty Measurement
                 }
                 else if (App.Instance.Mode is Mode.Edit)
                 {
